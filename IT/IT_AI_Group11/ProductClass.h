cbc5279c07b19bbc84286bf8c8fc1b6203f24bcf
aakwadwo1 <kwadwo.asumadu@acity.edu.gh>
2024-11-23 15:56:11 +0000

    AdminClass.h
    added a class for the admin, included a constructor to display predefined products

--- IT/IT_AI_Group11/ProductClass.h
+++ IT/IT_AI_Group11/ProductClass.h
@@ -3,12 +3,19 @@
 using namespace std;
 
 class Product{
+
 private:
     string name;
     int quantity;
     double price;
+
 public:
-    void setNAme(string n){
+    Product() : name(""), quantity(0), price(0.0) {
+    }
+    
+    Product(string n, int q, double p) : name(n), quantity(q), price(p) {  
+    }
+    void setName(string n){
         name = n;
         }
     void setQuantity(int q){
