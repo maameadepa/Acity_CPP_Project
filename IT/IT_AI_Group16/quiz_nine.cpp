3ba9f67ebf685898cc75235418c72b0fa3cc5486
Robert-Owoo <owoorobertgeorge@gmail.com>
2024-11-28 01:19:50 +0000

    add employee

--- IT/IT_AI_Group16/quiz_nine.cpp
+++ IT/IT_AI_Group16/quiz_nine.cpp
@@ -1,8 +1,38 @@
 #include<iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main (){
-    cout<< "my name is Robert";
+    struct Employee {
+    int id;
+    string name;
+    int age;
+    string dateOfBirth;
+    string gender;
+    string department;
+};
+
+void addEmployee(vector<Employee>& employees) {
+    Employee emp;
+    cout << "Enter Employee ID: ";
+    cin >> emp.id;
+    cin.ignore();
+    cout << "Enter Name: ";
+    getline(cin, emp.name);
+    cout << "Enter Age: ";
+    cin >> emp.age;
+    cin.ignore();
+    cout << "Enter Date of Birth (DD/MM/YYYY): ";
+    getline(cin, emp.dateOfBirth);
+    cout << "Enter Gender: ";
+    getline(cin, emp.gender);
+    cout << "Enter Department: ";
+    getline(cin, emp.department);
+    employees.push_back(emp);
+    cout << "Employee added successfully.\n";
+}
     
 }
 
--- IT/IT_AI_Group18/README.md
+++ IT/IT_AI_Group18/README.md
@@ -1,6 +1,8 @@
-# Acity_CPP_Project
-A repo for students in the C++ class to upload their projects which is in groups of 2.
+#include<iostream>
+using namespace std;
 
-# Group 1 - 22
+int main (){
+    
+}
 
 
