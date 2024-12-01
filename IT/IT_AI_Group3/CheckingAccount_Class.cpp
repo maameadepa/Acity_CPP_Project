87a568869a1735cd26a899e32de9a34d2a340db5
damilola ayodeji <damilolayodeji@gmail.com>
2024-12-01 15:52:16 +0000

    code 2.0
    finished with errors

--- IT/IT_AI_Group3/CheckingAccount_Class.cpp
+++ IT/IT_AI_Group3/CheckingAccount_Class.cpp
@@ -1,10 +1,27 @@
-#include<iostream>
+#include "Account_class.cpp" // Include the updated Account class
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
-// CheckingAccount.h
 
-class CheckingAccount{
+class CheckingAccount : public Account {
+public:
+    CheckingAccount(string accNum, string holder, double bal)
+        : Account(accNum, holder, bal) {}
 
-    public:
-     
-
+    void saveToFile() const override { // Overrides the virtual method
+        string filename = "CheckingAccount_" + getAccountNumber() + ".txt";
+        ofstream file(filename);
+        if (file.is_open()) {
+            file << "Account Type: Checking Account\n";
+            file << "Account Number: " << getAccountNumber() << "\n";
+            file << "Account Holder: " << getHolderName() << "\n";
+            file << "Balance: " << getBalance() << "\n";
+            file.close();
+            cout << "Account successfully saved to " << filename << ".\n";
+        } else {
+            cout << "Error saving account to file.\n";
+        }
+    }
 };
--- IT/IT_AI_Group3/MAIN_CODE.cpp
+++ IT/IT_AI_Group3/MAIN_CODE.cpp
@@ -1,27 +1,88 @@
-#include<iostream>
-#include<fstream>
-#include<ctime>
-#include"SavingsAccount_Class.cpp"
-#include"CheckingAccount_Class.cpp"
-#include"Account_class.cpp"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include "Account_class.cpp"
+#include "SavingsAccount_class.cpp"
+#include "CheckingAccount_class.cpp"
 
 using namespace std;
 
-  int main(){
+void displayMenu();
+void performOperation(Account* account, vector<Account*>& accounts);
+Account* findAccount(const vector<Account*>& accounts, const string& accountNumber);
+void logTransaction(const string& sender, const string& receiver, double amount);
+void viewTransactionHistory();
 
-  }
+int main() {
+    vector<Account*> accounts;
+    int choice;
 
+    do {
+        displayMenu();
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    
-// to display the menu
-/*
-void displayMenu() {
-    cout << "\n==== Bank Account Management ====\n";
-    cout << "1. Create Savings Account\n";
-    cout << "2. Create Checking Account\n";
-    cout << "3. Access Existing Account (Deposit/Withdraw/Check Balance/Send Money)\n";
-    cout << "4. View Transaction History\n";
-    cout << "5. Exit\n";
-    cout << "=================================\n";
+        string accNum, holder;
+        double initialBalance;
+        Account* account = nullptr;
+
+        switch (choice) {
+        case 1: {
+            cout << "Create Savings Account:\n";
+            cout << "Enter Account Number: ";
+            cin >> accNum;
+            cout << "Enter Account Holder Name: ";
+            cin.ignore();
+            getline(cin, holder);
+            cout << "Enter Initial Balance: ";
+            cin >> initialBalance;
+            account = new SavingsAccount(accNum, holder, initialBalance);
+            accounts.push_back(account);
+            account->saveToFile();
+            cout << "Savings Account created successfully!\n";
+            break;
+        }
+        case 2: {
+            cout << "Create Checking Account:\n";
+            cout << "Enter Account Number: ";
+            cin >> accNum;
+            cout << "Enter Account Holder Name: ";
+            cin.ignore();
+            getline(cin, holder);
+            cout << "Enter Initial Balance: ";
+            cin >> initialBalance;
+            account = new CheckingAccount(accNum, holder, initialBalance);
+            accounts.push_back(account);
+            account->saveToFile();
+            cout << "Checking Account created successfully!\n";
+            break;
+        }
+        case 3: {
+            cout << "Enter Account Number to access: ";
+            cin >> accNum;
+            account = findAccount(accounts, accNum);
+            if (account != nullptr) {
+                performOperation(account, accounts);
+            } else {
+                cout << "Account not found!\n";
+            }
+            break;
+        }
+        case 4:
+            viewTransactionHistory();
+            break;
+        case 5:
+            cout << "Exiting program.\n";
+            break;
+        default:
+            cout << "Invalid choice! Try again.\n";
+        }
+    } while (choice != 5);
+
+    for (Account* acc : accounts) {
+        delete acc;
+    }
+
+    return 0;
 }
-*/
