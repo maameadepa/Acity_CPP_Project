0addcada0051332de093de16db755e0a6ea761a9
kkjr7 <korantengnanaasare@gmail.com>
2024-12-01 18:53:20 +0000

    report card
    added functions and classes

--- CS/CS_Group4/project/project.cpp
+++ CS/CS_Group4/project/project.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 #include <fstream>
 using namespace std;
+
+// Remarks class to handle remarks based on average score
+class Remarks {
+private:
+    string remark;
+
+public:
+    void setRemark(float average) {
+        if (average >= 90) {
+            remark = "Excellent performance. More vim.";
+        } else if (average >= 75) {
+            remark = "Good job. You force.";
+        } else if (average >= 50) {
+            remark = "You try. There's room for improvement.";
+        } else {
+            remark = "Needs improvement. Don't give up.";
+        }
+    }
+
+    string getRemark() {
+        return remark;
+    }
+};
+
+// Function to calculate grades
+char calculateGrade(float subjectTotal) {
+    if (subjectTotal >= 90) {
+        return 'A';
+    } else if (subjectTotal >= 75) {
+        return 'B';
+    } else if (subjectTotal >= 50) {
+        return 'C';
+    } else {
+        return 'D';
+    }
+}
 
 int main() {
     string name, course;
@@ -9,7 +45,9 @@ int main() {
     float total = 0, average;
 
     const int MAX_SUBJECTS = 8;
-    string subjects[MAX_SUBJECTS]; 
+    string subjects[MAX_SUBJECTS];
+    float classScores[MAX_SUBJECTS], examScores[MAX_SUBJECTS];
+
     ofstream outToFile("reportcard.txt", ios::app);
 
     cout << "Enter student's name: ";
@@ -71,13 +109,11 @@ int main() {
     outToFile << "Roll Number: " << rollNumber << endl;
     outToFile << "Course     : " << course << endl << endl;
 
-    // Collect marks for each subject
-    for (int i = 0; i < MAX_SUBJECTS; ++i) {
+    for (int i = 0; i < MAX_SUBJECTS; i++) {
         float classScore, examScore, subjectTotal;
 
         cout << "Enter class score for " << subjects[i] << " (out of 40): ";
         cin >> classScore;
-
         while (classScore < 0 || classScore > 40) {
             cout << "Invalid score! Please enter a value between 0 and 40: ";
             cin >> classScore;
@@ -85,52 +121,36 @@ int main() {
 
         cout << "Enter exam score for " << subjects[i] << " (out of 60): ";
         cin >> examScore;
-
         while (examScore < 0 || examScore > 60) {
             cout << "Invalid score! Please enter a value between 0 and 60: ";
             cin >> examScore;
         }
 
-        subjectTotal = classScore + examScore; // Total score for the subject
+        classScores[i] = classScore;
+        examScores[i] = examScore;
+        subjectTotal = classScore + examScore;
         total += subjectTotal;
 
-        
-        char grade;
-        if (subjectTotal >= 90) {
-            grade = 'A';
-        } else if (subjectTotal >= 75) {
-            grade = 'B';
-        } else if (subjectTotal >= 50) {
-            grade = 'C';
-        } else {
-            grade = 'D';
-        }
+        // Use the calculateGrade function
+        char grade = calculateGrade(subjectTotal);
 
         outToFile << subjects[i] << "\t: " << subjectTotal << " | Grade: \t\t " << grade << endl;
     }
 
-    
     average = total / MAX_SUBJECTS;
+
+    // Use the Remarks class
+    Remarks remarkHandler;
+    remarkHandler.setRemark(average);
+    string remarks = remarkHandler.getRemark();
+
     outToFile << "\nTotal Marks: " << total << endl;
-    outToFile << "Average    : " << average << endl;
-
-    string remarks;
-    if (average >= 90) {
-        remarks = "Excellent performance. More vim.";
-    } else if (average >= 75) {
-        remarks = "Good job. You force.";
-    } else if (average >= 50) {
-        remarks = "You try. There's room for improvement.";
-    } else {
-        remarks = "Needs improvement. Don't give up.";
-    }
-
+    outToFile << "CWA        : " << average << endl;
     outToFile << "Remarks    : " << remarks << endl;
-
 
     cout << "\n--- Summary ---\n";
     cout << "Total Marks: " << total << endl;
-    cout << "CWA    : " << average << endl;
+    cout << "CWA        : " << average << endl;
     cout << "Remarks    : " << remarks << endl;
 
     outToFile.close();
